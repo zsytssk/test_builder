9d71031d98ac24b19ab86f68ebf007bc5cdd81e7
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-07-31 09:19:29 +0800

    master:[update]

--- script/main.ts
+++ script/main.ts
@@ -1,6 +1,7 @@
 import { cp } from './ls/main';
 
 const dist = 'Z:\\test\\testBuilder';
+const dist2 = 'D:\\zsytssk\\github\\HonorLite\\test\\testBuilder';
 const src = 'D:\\zsytssk\\github\\test_builder\\src';
 
 const type = process.argv.slice(2)[0];
@@ -8,6 +9,8 @@ async function main() {
     const actions = {
         release,
         syncBack,
+        release2,
+        syncBack2,
     };
     if (actions[type]) {
         await actions[type]();
@@ -18,7 +21,13 @@ main();
 async function release() {
     cp(src, dist);
 }
+async function release2() {
+    cp(src, dist2);
+}
 
 async function syncBack() {
     cp(dist, src);
+}
+async function syncBack2() {
+    cp(dist2, src);
 }
